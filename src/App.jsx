06fdc19433b7f818307c925ef9bce391c37757a6
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-11 01:21:04 +0530

    feat: React Routes implemented.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="user/:username" element={<Details />} />
+    </Routes>
+  );
+};
+
+export default App;
