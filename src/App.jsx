838bc94430e07816a63444131f792841e1f126ea
deeepak-me <deepak.krishnan.e@gmail.com>
2024-02-12 01:39:13 +0530

    refactor: code removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
